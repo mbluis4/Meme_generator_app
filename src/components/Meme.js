e37161f406864e4649a53f7fcfcf32e8887c831e
mbluis4 <92177912+mbluis4@users.noreply.github.com>
2022-02-10 18:50:38 -0300

    include texts
    include top and bottom text

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -7,8 +7,19 @@ export default function Meme() {
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+  console.log(meme.bottomText);
 
   const [allMemeImages, setAllMemeImages] = useState(memes);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMeme((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
 
   const getMemeImage = () => {
     const randNumber = Math.floor(
@@ -29,18 +40,32 @@ export default function Meme() {
           type="text"
           className="form--input"
           placeholder="top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         ></input>
         <input
           type="text"
           className="form--input"
           placeholder="bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         ></input>
 
         <button className="form--button" onClick={getMemeImage}>
           Get a new Meme Image
         </button>
       </div>
-      <img src={meme.randomImage} alt="meme"></img>
+      <div className="meme">
+        <img
+          src={meme.randomImage}
+          alt="random meme"
+          className="meme--image"
+        ></img>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
